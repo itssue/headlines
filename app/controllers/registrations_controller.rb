8fb5339a04fe0ed518661fd391577e6126e19356
itssue <its.sue@gmail.com>
2015-12-07 17:22:46 -0500

    mobile number required, redirects after sign in, send text on articles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def sign_up_params
+    params.require(:user).permit(:mobile_number, :email, :password)
+  end
+
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
